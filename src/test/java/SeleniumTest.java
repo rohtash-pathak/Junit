6f414552438d6a29a915a58595263c47457f1109
qxz0gbo <rohtash.pathak@partner.bmw.de>
2020-06-12 14:48:04 +0530

    testing selenium

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class SeleniumTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void seleniumTest(){
+        driver.navigate().to("https://theindianaura.com/");
+        String title = driver.getTitle();
+        assertTrue(title.contains("The Indian Aura"));
+    }
+}
